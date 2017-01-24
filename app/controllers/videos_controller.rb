f58a17dd5756e78d88b194ccb1f9e10c5fb20df8
Marvin <mrm3101@gmail.com>
2017-01-24 00:50:07 +0000

    showing articles and videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -5,13 +5,21 @@ class VideosController < ApplicationController
     
     def create 
         #render plain: params[:video].inspect
-        @video = Video.new(article_params)
-        @video.save
-        redirect_to_article_show(@article)
+        @video = Video.new(video_params)
+        if @video.save
+            flash[:notice] = "Video was sucessfully created"
+            redirect_to video_path(@video)
+        else 
+            render 'new'
+        end 
+    end
+    
+    def show 
+        @video = Video.find(params[:id])
     end
     
     private 
-      def article_params
+      def video_params
           params.require(:video).permit(:title, :description)
       end
 end
